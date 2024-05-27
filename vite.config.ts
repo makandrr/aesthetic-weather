16fa1b27c305667b71784c60c37bf7234195533c
Maksim Andriushin <maximdevtool@gmail.com>
2024-05-27 21:49:18 +0300

    GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@scss': path.resolve(__dirname, './src/scss')
-    }
-  },
+  base: '/aesthetic-weather/',
+  resolve: {},
   plugins: [react()],
 })
